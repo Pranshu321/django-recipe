f31f3aa28d74ebe95d418416878622fbec7e52aa
Pranshu Jain <86917304+Pranshu321@users.noreply.github.com>
2023-10-27 17:41:42 +0530

    Update settings.py

--- modeldjango/settings.py
+++ modeldjango/settings.py
@@ -140,5 +140,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pranshujain0111@gmail.com'
-EMAIL_HOST_PASSWORD = 'Pranshu-2002'
